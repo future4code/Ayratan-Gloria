936b1664c505e048d3588afaacbfc23a3d8769ee
Ayratan <70165987+Ayratan@users.noreply.github.com>
2021-08-12 19:20:08 -0300

    Exercicio 6

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -40,7 +40,19 @@ function retornaQuantidadeElementos(array) {
 
 // EXERCÍCIO 06
 function retornaExpressoesBooleanas() {
+  const booleano1 = true
+  const booleano2 = false
+  const booleano3 = !booleano2 
+  const booleano4 = !booleano3 
+//armazenando os valores do calculo logico em variaveis para facilitar o acesso
+  const a = booleano1 && booleano2 && !booleano4
+  const b = (booleano1 && booleano2) || !booleano3
+  const c = (booleano2 || booleano3) && (booleano4 || booleano1)
+  const d = !(booleano2 && booleano3) || !(booleano1 && booleano3)
+  const e = !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)
 
+  const resposta = [a, b , c , d, e]
+  return resposta
 }
 
 // EXERCÍCIO 07
